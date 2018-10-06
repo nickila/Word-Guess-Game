0abce6b07e19593fc0a43d19c35ad23646335b28
Brian Nickila <brian.nickila@gmail.com>
2018-10-06 18:24:20 -0500

    trying to prevent multiple wrong letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,27 +10,43 @@ var wins = 0;
 var strL = "";
 var groupedLetters = "";
 
+// var hasEl = function(str, char) {
+//     for (var i = 0; i < str.length; i ++) {
+//         if (str[i] == char) {
+//             return true;
+//         }
+//     }
+//     return false;
+// };
+//console.log(hasEl(groupedLetters, userGuess));
+
+
+
+
+
+
+
 function clearBox() {
-document.getElementById("wrongBoxL").innerHTML = "";
+    document.getElementById("wrongBoxL").innerHTML = "";
 
 }
-function clearSpan(){
-document.getElementById("letter0").innerHTML = "";
-document.getElementById("letter1").innerHTML = "";
-document.getElementById("letter2").innerHTML = "";
-document.getElementById("letter3").innerHTML = "";
-document.getElementById("letter4").innerHTML = "";
-document.getElementById("letter5").innerHTML = "";
-document.getElementById("letter0").style.color = "yellow";
-document.getElementById("letter1").style.color = "yellow";
-document.getElementById("letter2").style.color = "yellow";
-document.getElementById("letter3").style.color = "yellow";
-document.getElementById("letter4").style.color = "yellow";
-document.getElementById("letter5").style.color = "yellow";
-document.getElementById("spanString").style.marginTop = "200px";
+function clearSpan() {
+    document.getElementById("letter0").innerHTML = "";
+    document.getElementById("letter1").innerHTML = "";
+    document.getElementById("letter2").innerHTML = "";
+    document.getElementById("letter3").innerHTML = "";
+    document.getElementById("letter4").innerHTML = "";
+    document.getElementById("letter5").innerHTML = "";
+    document.getElementById("letter0").style.color = "yellow";
+    document.getElementById("letter1").style.color = "yellow";
+    document.getElementById("letter2").style.color = "yellow";
+    document.getElementById("letter3").style.color = "yellow";
+    document.getElementById("letter4").style.color = "yellow";
+    document.getElementById("letter5").style.color = "yellow";
+    document.getElementById("spanString").style.marginTop = "200px";
 }
 
-function clearMessage2(){
+function clearMessage2() {
     document.getElementById("message2").innerHTML = "";
 }
 
@@ -54,16 +70,16 @@ wordGame();
 
 
 
-function wordGame() { 
-    guesses=10; 
+function wordGame() {
+    guesses = 10;
     clearSpan();
     clearBox();
     clearMessage2();
     document.getElementById("guesses").innerHTML = "GUESS: " + guesses;
-    
-  
-//2. Randomly choose one of those words.
-//function hangman() {
+
+
+    //2. Randomly choose one of those words.
+    //function hangman() {
     var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
     console.log(randomWord);
     //3. Put that random word into a new string. (not needed)
@@ -123,14 +139,33 @@ function wordGame() {
         console.log(userGuess);
         groupedLetters = groupedLetters + userGuess;
 
+        // var hasEl = function(str, char) {
+        //     for (var i = 0; i < str.length; i ++) {
+        //         if (str[i] == char) {
+        //             return true;
+        //         }
+        //     }
+        //     return false;
+        // };
+        // console.log(hasEl(groupedLetters, userGuess));
+
+
+
+
+
         console.log("GL" + groupedLetters);
+        var usedOnes = groupedLetters.search(userGuess);
+        console.log("usedOnes: " + usedOnes);
         //var letter = (userGuess);
 
         var alphabet = "abcdefghijklmnopqrstuvwxyz";
         var checkLetter = alphabet.search(userGuess);
 
-        console.log(checkLetter);
+        console.log("checkLetter: " + checkLetter);
 
+
+
+        //console.log("NUMBER: " + groupedLetters.length);
 
 
 
@@ -199,21 +234,21 @@ function wordGame() {
 
             document.getElementById("message2").innerHTML = "Click here to play again...";
             document.getElementById("spanString").style.marginTop = "160px";
-            
-            
-            
-            
+
+
+
+
 
             //document.getElementById('endereco').onkeydown = function(event) {
-               // if (event.keyCode == 13) {
-                 //   myGame()
-               // }
-           // }
+            // if (event.keyCode == 13) {
+            //   myGame()
+            // }
+            // }
             //myGame();
             //}
             return;
-            
-            
+
+
         }
         //Not sure if this will work, but my thought is I can put every wrong letter into a string and check
         //to be sure it's not in that string before it gets marked against the player. So str1 will be the 
@@ -226,78 +261,89 @@ function wordGame() {
         //     var patt = new RegExp("e");
         //     var res = patt.test(str);
         //     document.getElementById("demo").innerHTML = res;
-       // }
-        
-        
+        // }
+
+
+
+
 
         //7. If that letter is not in that string, place it into another div that shows wrong letters.
-        if ((checkLetter >= 0 && checkLetter < alphabet.length && point !== (wordLength + 1))
-            && (userGuess !== wordLetter0)
-            && (userGuess !== wordLetter1)
-            && (userGuess !== wordLetter2)
-            && (userGuess !== wordLetter3)
-            && (userGuess !== wordLetter4)
-            && (userGuess !== wordLetter5)
-            //&& (userGuess !== groupedLetters)
-            && (userGuess !== groupedLetters.indexOf())
-            //&& (userGuess !== "wrongBoxL LI")
-            && (userGuess != str1)){
-            // Div containing what happens when a wrong choice is submitted.
-            var usedLetter = str1.concat(userGuess);
-            console.log ("usedLetter=" + usedLetter);
-            document.getElementById("message").innerHTML = "Nope. Try again...";
-            var node = document.createElement("LI");                 // Create a <li> node
-            var textnode = document.createTextNode(userGuess);         // Create a text node
-            node.appendChild(textnode);                              // Append the text to <li>
-            document.getElementById("wrongBoxL").appendChild(node);     // Append <li> to <ul> with id
-            document.getElementById("guesses").innerHTML = "GUESS: 0" + (guesses - 1);
-            //8. Also, if letter does not equal any from the string, minus one point (or add a mark).
-            guesses--;
-            //var str = document.getElementById("wordBoxL").innerHTML();
-            //console.log(str);
-            //put wrong letters into span
-            //9. After 6 wrong guesses, player loses. Reveal word and 'Game Over'.
-            if (guesses === 0) {
-                document.getElementById("title").innerHTML = "GAME OVER";
-                document.getElementById("message").innerHTML = "You lost...";
-                document.getElementById("letter0").innerHTML = wordLetter0;
-                document.getElementById("letter1").innerHTML = wordLetter1;
-                document.getElementById("letter2").innerHTML = wordLetter2;
-                document.getElementById("letter3").innerHTML = wordLetter3;
-                document.getElementById("letter4").innerHTML = wordLetter4;
-                document.getElementById("letter5").innerHTML = wordLetter5;
-                // document.getElementById("spanString").innerHTML = wordLetter0 + " " + wordLetter1 + " " + wordLetter2 + " " + wordLetter3 + " " + wordLetter4 + " " + wordLetter5;
-                document.getElementById("spanString").style.color = "turquoise";
-                losses++;
-                if (losses < 10) {
-                document.getElementById("losses").innerHTML = " LOSS:0" + losses;
-
-                } else { document.getElementById("losses").innerHTML = " LOSS:" + losses;
-            }
-                document.getElementById("message2").innerHTML = "Click here to play again...";
-                document.getElementById("spanString").style.marginTop = "160px";
-                
-                // wordGame()
-                return;
-                
-            }
-        }
-
-    }
-    
-//}
-
-//document.onclick(event.key)
 
 
+
+        // var hasEl = function (str, char) {
+        //     for (var i = 0; i < str.length; i++) {
+        //         if (str[i] !== char) {
+
+
+                    if ((checkLetter >= 0 && checkLetter < alphabet.length && point !== (wordLength + 1))
+                        && (userGuess !== wordLetter0)
+                        && (userGuess !== wordLetter1)
+                        && (userGuess !== wordLetter2)
+                        && (userGuess !== wordLetter3)
+                        && (userGuess !== wordLetter4)
+                        && (userGuess !== wordLetter5)
+                        //&& (userGuess !== groupedLetters)
+                        && (userGuess !== groupedLetters.indexOf())
+                        //&& (userGuess !== "wrongBoxL LI")
+                        && (userGuess != str1)) {
+                           // console.log("hasEl:" + hasEl(groupedLetters, userGuess));
+                        // Div containing what happens when a wrong choice is submitted.
+                        var usedLetter = str1.concat(userGuess);
+                        console.log("usedLetter=" + usedLetter);
+                        document.getElementById("message").innerHTML = "Nope. Try again...";
+                        var node = document.createElement("LI");                 // Create a <li> node
+                        var textnode = document.createTextNode(userGuess);         // Create a text node
+                        node.appendChild(textnode);                              // Append the text to <li>
+                        document.getElementById("wrongBoxL").appendChild(node);     // Append <li> to <ul> with id
+                        document.getElementById("guesses").innerHTML = "GUESS: 0" + (guesses - 1);
+                        //8. Also, if letter does not equal any from the string, minus one point (or add a mark).
+                        guesses--;
+                        
+                        //var str = document.getElementById("wordBoxL").innerHTML();
+                        //console.log(str);
+                        //put wrong letters into span
+                        //9. After 6 wrong guesses, player loses. Reveal word and 'Game Over'.
+                        if (guesses === 0) {
+                            document.getElementById("title").innerHTML = "GAME OVER";
+                            document.getElementById("message").innerHTML = "You lost...";
+                            document.getElementById("letter0").innerHTML = wordLetter0;
+                            document.getElementById("letter1").innerHTML = wordLetter1;
+                            document.getElementById("letter2").innerHTML = wordLetter2;
+                            document.getElementById("letter3").innerHTML = wordLetter3;
+                            document.getElementById("letter4").innerHTML = wordLetter4;
+                            document.getElementById("letter5").innerHTML = wordLetter5;
+                            // document.getElementById("spanString").innerHTML = wordLetter0 + " " + wordLetter1 + " " + wordLetter2 + " " + wordLetter3 + " " + wordLetter4 + " " + wordLetter5;
+                            document.getElementById("spanString").style.color = "turquoise";
+                            losses++;
+                            if (losses < 10) {
+                                document.getElementById("losses").innerHTML = " LOSS:0" + losses;
+
+                            } else {
+                                document.getElementById("losses").innerHTML = " LOSS:" + losses;
+                            }
+                            document.getElementById("message2").innerHTML = "Click here to play again...";
+                            document.getElementById("spanString").style.marginTop = "160px";
+                            
+                            // wordGame()
+                            return;
+
+                        } 
+                        }
+                    }
+
+                }
+
+            
+
+            //document.onclick(event.key)
+
+        
         //11. If user wins: if player chooses all letters that are in the word, flash win and return.
 
 
+    
 
 
 
         //10. Reset button chooses new word and resets everything back to beginning.
-}
-
-
-
